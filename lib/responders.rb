357b7101e04497bbce11dfeec4fd4b0104880a7c
José Valim <jose.valim@gmail.com>
2010-03-30 11:00:13 +0200

    Make it compatible with master.

--- lib/responders.rb
+++ lib/responders.rb
@@ -8,17 +8,16 @@ module Responders
   ActionController::Base.extend Responders::ControllerMethod
 
   class Railtie < ::Rails::Railtie
-    railtie_name :responders
-
+    config.responders = ActiveSupport::OrderedOptions.new
     config.generators.scaffold_controller = :responders_controller
 
     # Add load paths straight to I18n, so engines and application can overwrite it.
     require 'active_support/i18n'
     I18n.load_path << File.expand_path('../responders/locales/en.yml', __FILE__)
 
-    initializer "responders.flash_responder" do
-      if config.responders.flash_keys
-        Responders::FlashResponder.flash_keys = config.responders.flash_keys 
+    initializer "responders.flash_responder" do |app|
+      if app.config.responders.flash_keys
+        Responders::FlashResponder.flash_keys = app.config.responders.flash_keys
       end
     end
   end
