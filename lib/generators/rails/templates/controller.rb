04fd60211a2366a6a77419e5f64bd3630d0ee958
Gabriel Paladino <gabriel@scupen.com>
2014-08-15 11:31:38 -0300

    changing controller template

--- lib/generators/rails/templates/controller.rb
+++ lib/generators/rails/templates/controller.rb
@@ -1,55 +1,54 @@
+<% if namespaced? -%>
+require_dependency "<%= namespaced_file_path %>/application_controller"
+
+<% end -%>
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < ApplicationController
-  <%= controller_before_filter %> :set_<%= file_name %>, only: [:show, :edit, :update, :destroy]
+  before_action :set_<%= singular_table_name %>, only: [:show, :edit, :update, :destroy]
 
-<% unless options[:singleton] -%>
   def index
-    @<%= table_name %> = <%= orm_class.all(class_name) %>
-    respond_with(@<%= table_name %>)
+    @<%= plural_table_name %> = <%= class_name %>.all
+    respond_with(@<%= plural_table_name %>)
   end
-<% end -%>
 
   def show
-    respond_with(@<%= file_name %>)
+    respond_with(@<%= singular_table_name %>)
   end
 
   def new
-    @<%= file_name %> = <%= orm_class.build(class_name) %>
-    respond_with(@<%= file_name %>)
+    @<%= singular_table_name %> = <%= class_name %>.build
+    respond_with(@<%= singular_table_name %>)
   end
 
   def edit
   end
 
   def create
-    @<%= file_name %> = <%= orm_class.build(class_name, attributes_params) %>
-    <%= "flash[:notice] = '#{class_name} was successfully created.' if " if flash? %>@<%= orm_instance.save %>
-    respond_with(@<%= file_name %>)
+    @<%= singular_table_name %> = <%= class_name %>.create(<%= singular_table_name %>_params)
+    respond_with(@<%= singular_table_name %>)
   end
 
   def update
-    <%= "flash[:notice] = '#{class_name} was successfully updated.' if " if flash? %>@<%= orm_instance_update(attributes_params) %>
-    respond_with(@<%= file_name %>)
+    @<%= singular_table_name %>.update(<%= singular_table_name %>_params)
+    respond_with(@<%= singular_table_name %>)
   end
 
   def destroy
-    @<%= orm_instance.destroy %>
-    respond_with(@<%= file_name %>)
+    @<%= singular_table_name %>.destroy
+    respond_with(@<%= singular_table_name %>)
   end
 
   private
-    def set_<%= file_name %>
-      @<%= file_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    def set_<%= singular_table_name %>
+      @<%= singular_table_name %> = <%= class_name %>.find(params[:id])
     end
-    <%- if strong_parameters_defined? -%>
 
-    def <%= "#{file_name}_params" %>
+    def <%= "#{singular_table_name}_params" %>
       <%- if attributes_names.empty? -%>
-      params[:<%= file_name %>]
+      params[:<%= singular_table_name %>]
       <%- else -%>
-      params.require(:<%= file_name %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
+      params.require(:<%= singular_table_name %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
       <%- end -%>
     end
-    <%- end -%>
 end
 <% end -%>
