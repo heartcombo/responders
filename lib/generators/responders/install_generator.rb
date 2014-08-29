1d2294791f23b33ee005644c0f93f1766908e3ca
HoaiAn <hoaianr@gmail.com>
2014-08-29 11:33:57 +0700

    Add strip_here_doc for indentation issuse when creating file

--- lib/generators/responders/install_generator.rb
+++ lib/generators/responders/install_generator.rb
@@ -4,26 +4,24 @@ module Responders
       source_root File.expand_path("..", __FILE__)
 
       desc "Creates an initializer with default responder configuration and copy locale file"
-
       def create_responder_file
-        create_file "lib/application_responder.rb", <<-RUBY
-class ApplicationResponder < ActionController::Responder
-  include Responders::FlashResponder
-  include Responders::HttpCacheResponder
+        create_file "lib/application_responder.rb", <<-RUBY.strip_heredoc
+          class ApplicationResponder < ActionController::Responder
+            include Responders::FlashResponder
+            include Responders::HttpCacheResponder
 
-  # Redirects resources to the collection path (index action) instead
-  # of the resource path (show action) for POST/PUT/DELETE requests.
-  # include Responders::CollectionResponder
-end
+            # Redirects resources to the collection path (index action) instead
+            # of the resource path (show action) for POST/PUT/DELETE requests.
+            # include Responders::CollectionResponder
+          end
         RUBY
       end
 
       def update_application_controller
         prepend_file "app/controllers/application_controller.rb", %{require "application_responder"\n\n}
-        inject_into_class "app/controllers/application_controller.rb", "ApplicationController", <<-RUBY
-  self.responder = ApplicationResponder
-  respond_to :html
-
+        inject_into_class "app/controllers/application_controller.rb", "ApplicationController", <<-RUBY.strip_heredoc
+          self.responder = ApplicationResponder
+          respond_to :html
         RUBY
       end
 
