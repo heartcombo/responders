37eeca418fd7fd41059956a2478d29b3d6fec3f1
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2021-01-13 18:45:09 -0300

    Do not rely on ActiveSupport::JSON being loaded for this test model
    Without it we're getting a couple errors with Ruby 3.0 and Rails 6 that
    I couldn't track down on a quick search, but at the end of the day we
    shouldn't be relying on ActiveSupport::JSON anyway to be present, we
    just want to test that the model responds/serializes appropriately to
    json method like the other methods.

--- test/support/models.rb
+++ test/support/models.rb
@@ -4,6 +4,10 @@ class Customer < Struct.new(:name, :id)
   extend ActiveModel::Naming
   include ActiveModel::Conversion
 
+  def to_json(*)
+    to_h.to_json
+  end
+
   def to_xml(options = {})
     if options[:builder]
       options[:builder].name name
