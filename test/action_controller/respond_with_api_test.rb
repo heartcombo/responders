a393f550a1295c20fd7ebd4fd4023bf68f29d210
Rafael Mendonça França <rafael@franca.dev>
2022-03-25 20:19:41 +0000

    ActionController::Api is always defined in the versions we test

--- test/action_controller/respond_with_api_test.rb
+++ test/action_controller/respond_with_api_test.rb
@@ -3,44 +3,42 @@
 require "test_helper"
 require "support/models"
 
-if defined?(ActionController::API)
-  class ApiRespondWithController < ActionController::API
-    respond_to :json
+class ApiRespondWithController < ActionController::API
+  respond_to :json
 
-    def index
-      respond_with [
-        Customer.new("Foo", 1),
-        Customer.new("Bar", 2),
-      ]
-    end
-
-    def create
-      respond_with Customer.new("Foo", 1), location: "http://test.host/"
-    end
+  def index
+    respond_with [
+      Customer.new("Foo", 1),
+      Customer.new("Bar", 2),
+    ]
   end
 
-  class RespondWithAPITest < ActionController::TestCase
-    tests ApiRespondWithController
+  def create
+    respond_with Customer.new("Foo", 1), location: "http://test.host/"
+  end
+end
 
-    def test_api_controller_without_view_rendering
-      @request.accept = "application/json"
+class RespondWithAPITest < ActionController::TestCase
+  tests ApiRespondWithController
 
-      get :index
-      assert_equal 200, @response.status
-      expected = [{ name: "Foo", id: 1 }, { name: "Bar", id: 2 }]
-      assert_equal expected.to_json, @response.body
+  def test_api_controller_without_view_rendering
+    @request.accept = "application/json"
 
-      post :create
-      assert_equal 201, @response.status
-      expected = { name: "Foo", id: 1 }
-      assert_equal expected.to_json, @response.body
+    get :index
+    assert_equal 200, @response.status
+    expected = [{ name: "Foo", id: 1 }, { name: "Bar", id: 2 }]
+    assert_equal expected.to_json, @response.body
 
-      errors = { name: ["invalid"] }
-      Customer.any_instance.stubs(:errors).returns(errors)
-      post :create
-      assert_equal 422, @response.status
-      expected = { errors: errors }
-      assert_equal expected.to_json, @response.body
-    end
+    post :create
+    assert_equal 201, @response.status
+    expected = { name: "Foo", id: 1 }
+    assert_equal expected.to_json, @response.body
+
+    errors = { name: ["invalid"] }
+    Customer.any_instance.stubs(:errors).returns(errors)
+    post :create
+    assert_equal 422, @response.status
+    expected = { errors: errors }
+    assert_equal expected.to_json, @response.body
   end
 end
